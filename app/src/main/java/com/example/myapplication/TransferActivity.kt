97123b96c372818579d692fda7e4dcae2a465f17
olzeev <olzeevmax@gmail.com>
2023-03-14 03:07:53 +0300

    Transfer page added with 2 inputboxes and a button

--- app/src/main/java/com/example/myapplication/TransferActivity.kt
+++ app/src/main/java/com/example/myapplication/TransferActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class TransferActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
+    }
+
+    fun makeTransferClick(view: View){
+        val newToast = Toast.makeText(this, "Переведено", Toast.LENGTH_SHORT)
+        newToast.show()
     }
 }
