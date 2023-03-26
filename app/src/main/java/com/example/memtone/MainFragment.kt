0ac78e986d470b008f7015377aaf1cd00a9d5ecd
Ilyas <93606521+IlyasIshmullin@users.noreply.github.com>
2023-03-26 16:56:45 +0300

    Update app/src/main/java/com/example/memtone/MainFragment.kt
    Co-authored-by: Ilya Timofeev <waterlava535@gmail.com>

--- app/src/main/java/com/example/memtone/MainFragment.kt
+++ app/src/main/java/com/example/memtone/MainFragment.kt
@@ -48,7 +48,6 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        convertToDollars("1")
         binding.btnNFC.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_nfcFragment)
         }
